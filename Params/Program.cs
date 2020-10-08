18382d3a946ba10f1496d3e6a1e3df66941a11cf
Opilane TTHK <opilane@tthk.ee>
2020-10-08 11:26:23 +0300

    first commit

---
+++ Params/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParamsSum(50, 50);
+            DisplayString("some random string.");
+        }
+        //write function that takes in two numbers and displays
+        //their sum as a result
+        public static void ParamsSum(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+        //write a function that takes in a string as a parameter
+        //and displays it in console
+        public static void DisplayString(string hello)
+        {
+            Console.WriteLine(hello);
+        }
+    }
+}
